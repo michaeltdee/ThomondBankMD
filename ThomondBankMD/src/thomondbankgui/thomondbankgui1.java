f2a0029e4644fcbcd8b30f8bd2ac3784b782f15b
Michael Dee <dmikeyy123@gmail.com>
2025-03-28 10:11:55 +0000

    first gui

--- ThomondBankMD/src/thomondbankgui/thomondbankgui1.java
+++ ThomondBankMD/src/thomondbankgui/thomondbankgui1.java
@@ -1,4 +1,20 @@
 package thomondbankgui;
 
+import javax.swing.*;
+
 public class thomondbankgui1 {
+    private JPanel rootPanel;
+    private JTabbedPane tabbedPane1;
+    private JPanel bankOfficerTabbedPane;
+    private JPanel userTabbedPane;
+    private JLabel accountIdLbl;
+    private JTextField accountIdTxt;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("thomondbankgui1");
+        frame.setContentPane(new thomondbankgui1().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
