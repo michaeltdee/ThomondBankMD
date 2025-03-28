1594f552bb17a6cb289d108f00b62e6671057425
Michael Dee <dmikeyy123@gmail.com>
2025-03-28 10:45:36 +0000

    Bank officer tabbed pane

--- ThomondBankMD/src/thomondbankgui/thomondbankgui1.java
+++ ThomondBankMD/src/thomondbankgui/thomondbankgui1.java
@@ -9,6 +9,9 @@ public class thomondbankgui1 {
     private JPanel userTabbedPane;
     private JLabel accountIdLbl;
     private JTextField accountIdTxt;
+    private JButton createAccountBtn;
+    private JButton changeAIRBtn;
+    private JButton changeOverdraftLimitBtn;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("thomondbankgui1");
